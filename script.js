8b4c2af9d31261c057ddc31e88ef7b0d3e04daf4
Jamie <92243661+Jaorow@users.noreply.github.com>
2023-05-19 12:37:42 +1200

    popup box working

--- script.js
+++ script.js
@@ -11,4 +11,39 @@ function loaded() {
     setTimeout(flickerNeon, 2500);
     setTimeout(flickerNeon, 500);
     setTimeout(flickerNeon, 2000);
+
+    // for creating reg form for testing
+    triggerReg();
+}
+
+
+// for registration form
+var form_visible=false;
+
+function triggerReg() {
+    document.getElementById("form-ct").style.display = "block";
+    document.getElementById("center-picture").style.display = "none";
+    form_visible = true;
+    addEv();
+}
+
+function addEv(){
+    console.log("add event listner");
+    window.addEventListener('click', function(e){ 
+        if (
+            !document.getElementById('form-ct').contains(e.target) 
+            && form_visible 
+            && !document.getElementById('trigger-modal').contains(e.target)){
+
+            console.log("clicked outside reg box");
+            unTrigerReg();
+        }
+    });
+}
+
+function unTrigerReg() {
+    document.getElementById("form-ct").style.display = "none";
+    document.getElementById("center-picture").style.display = "block";
+
+    form_visible=false;
 }
